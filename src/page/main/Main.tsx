f0220e8ca49111a5109b29383d8158c91ce7b69b
Kim Ji Yu <spbabo97@naver.com>
2024-04-02 01:52:19 +0900

    :lipstick: 메인페이지 UI 생성

--- src/page/main/Main.tsx
+++ src/page/main/Main.tsx
@@ -1,5 +1,6 @@
 import {Swiper,SwiperSlide} from "swiper/react";
-import {Autoplay} from "swiper/modules"
+import {Autoplay, Navigation} from "swiper/modules"
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function Main() {
@@ -155,6 +156,191 @@ export default function Main() {
                 </Link>
             </SwiperSlide>
         </Swiper>
+
+        <div className="bg-white rounded-xl w-11/12 max-w-7xl mx-auto py-10 px-4 md:p-10 mt-20 md:mt-32">
+
+            <div className="flex justify-between">
+                <h4 className="text-center text-lg font-bold text-point-color md:text-2xl">제목</h4>
+                <div className="text-point-color text-3xl flex items-center">
+                    <button><FaAngleLeft/></button>
+                    <button><FaAngleRight/></button>
+                </div>
+            </div>
+
+            <div className="mt-3">
+                <Swiper
+                    slidesPerView={1.2}
+                    spaceBetween={10}
+                    loop={true}
+                    speed={600}
+                    modules={[Navigation]}
+                    /* navigation={{
+                        nextEl : rightRef.current,
+                        prevEl : leftRef.current
+                    }} */
+                    breakpoints={{
+                        481 : {
+                        slidesPerView : 2.3,
+                        spaceBetween : 20,
+                        },
+                        821 : {
+                        slidesPerView : 4,
+                        spaceBetween : 30,
+                        }
+                    }}
+                    >
+                    {
+                        <SwiperSlide>
+                            <Link to={`/movie/view/1`} className="group">
+                                <div
+                                    className={`w-full bg-cyan-50 relative overflow-hidden pb-[calc(470/329*100%)] after:pb-2.5 after:content-[''] after:block`}
+                                >
+                                    <img 
+                                        className="absolute left-0 top-0 w-full h-full object-cover transition-transform group-hover:scale-110 "
+                                        src="/image/poster.jpg"
+                                        alt="영화 포스터" 
+                                    />
+                                </div>
+                                <div className="mt-3">
+
+                                    <div>"NEW"</div>
+                                    <p className="text-xs text-point-color">순위</p>
+
+                                    <dl>
+                                        <dt className="text-base font-semibold break-keep">123</dt>
+                                        <dd className="text-sm mt-1 text-neutral-500">개봉일 : 0</dd>
+                                    </dl>
+                                    <p className="text-xs mt-1 text-neutral-500">누적 관객수 - 0 명</p>
+                                    
+                                </div>
+                            </Link>
+                        </SwiperSlide>
+                    }
+                </Swiper>
+            </div>
+
+            <div className="flex justify-between">
+                <h4 className="text-center text-lg font-bold text-point-color md:text-2xl">제목</h4>
+                <div className="text-point-color text-3xl flex items-center">
+                    <button><FaAngleLeft/></button>
+                    <button><FaAngleRight/></button>
+                </div>
+            </div>
+
+            <div className="mt-3">
+                <Swiper
+                    slidesPerView={1.2}
+                    spaceBetween={10}
+                    loop={true}
+                    speed={600}
+                    modules={[Navigation]}
+                    /* navigation={{
+                        nextEl : rightRef.current,
+                        prevEl : leftRef.current
+                    }} */
+                    breakpoints={{
+                        481 : {
+                        slidesPerView : 2.3,
+                        spaceBetween : 20,
+                        },
+                        821 : {
+                        slidesPerView : 4,
+                        spaceBetween : 30,
+                        }
+                    }}
+                    >
+                    {
+                        <SwiperSlide>
+                            <Link to={`/movie/view/1`} className="group">
+                                <div
+                                    className={`w-full bg-cyan-50 relative overflow-hidden pb-[calc(470/329*100%)] after:pb-2.5 after:content-[''] after:block`}
+                                >
+                                    <img 
+                                        className="absolute left-0 top-0 w-full h-full object-cover transition-transform group-hover:scale-110 "
+                                        src="/image/poster.jpg"
+                                        alt="영화 포스터" 
+                                    />
+                                </div>
+                                <div className="mt-3">
+
+                                    <div>"NEW"</div>
+                                    <p className="text-xs text-point-color">순위</p>
+
+                                    <dl>
+                                        <dt className="text-base font-semibold break-keep">123</dt>
+                                        <dd className="text-sm mt-1 text-neutral-500">개봉일 : 0</dd>
+                                    </dl>
+                                    <p className="text-xs mt-1 text-neutral-500">누적 관객수 - 0 명</p>
+                                    
+                                </div>
+                            </Link>
+                        </SwiperSlide>
+                    }
+                </Swiper>
+            </div>
+
+            <div className="flex justify-between">
+                <h4 className="text-center text-lg font-bold text-point-color md:text-2xl">제목</h4>
+                <div className="text-point-color text-3xl flex items-center">
+                    <button><FaAngleLeft/></button>
+                    <button><FaAngleRight/></button>
+                </div>
+            </div>
+
+            <div className="mt-3">
+                <Swiper
+                    slidesPerView={1.2}
+                    spaceBetween={10}
+                    loop={true}
+                    speed={600}
+                    modules={[Navigation]}
+                    /* navigation={{
+                        nextEl : rightRef.current,
+                        prevEl : leftRef.current
+                    }} */
+                    breakpoints={{
+                        481 : {
+                        slidesPerView : 2.3,
+                        spaceBetween : 20,
+                        },
+                        821 : {
+                        slidesPerView : 4,
+                        spaceBetween : 30,
+                        }
+                    }}
+                    >
+                    {
+                        <SwiperSlide>
+                            <Link to={`/movie/view/1`} className="group">
+                                <div
+                                    className={`w-full bg-cyan-50 relative overflow-hidden pb-[calc(470/329*100%)] after:pb-2.5 after:content-[''] after:block`}
+                                >
+                                    <img 
+                                        className="absolute left-0 top-0 w-full h-full object-cover transition-transform group-hover:scale-110 "
+                                        src="/image/poster.jpg"
+                                        alt="영화 포스터" 
+                                    />
+                                </div>
+                                <div className="mt-3">
+
+                                    <div>"NEW"</div>
+                                    <p className="text-xs text-point-color">순위</p>
+
+                                    <dl>
+                                        <dt className="text-base font-semibold break-keep">123</dt>
+                                        <dd className="text-sm mt-1 text-neutral-500">개봉일 : 0</dd>
+                                    </dl>
+                                    <p className="text-xs mt-1 text-neutral-500">누적 관객수 - 0 명</p>
+                                    
+                                </div>
+                            </Link>
+                        </SwiperSlide>
+                    }
+                </Swiper>
+            </div>
+            
+
+        </div>
 
     </main>
   )
