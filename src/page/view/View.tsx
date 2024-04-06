2d2a8dfe4e9e7158e72f962c3f594bfe55912fbf
Kim Ji Yu <spbabo97@naver.com>
2024-04-07 02:32:54 +0900

    :sparkles: 영화 포스터 가져오는 기능 추가

--- src/page/view/View.tsx
+++ src/page/view/View.tsx
@@ -1,33 +1,19 @@
 import { useParams } from "react-router-dom";
-import moment from "moment";
 import { useEffect } from "react";
 import { movieInfoFetch } from "../../utils/fetch/view/movieInfo";
 import { useQuery } from "@tanstack/react-query";
 import List from "../../components/review/List";
+import Poster from "../../components/view/Poster";
+import SmallPoster from "../../components/view/SmallPoster";
 
 export default function View() {
 
     const {movieCd} = useParams();
-    // const [poster,setPoster] = useState({});
 
     const {data : info} = useQuery({
         queryKey : ["infoData",movieCd],
         queryFn : ()=>movieInfoFetch(movieCd || "")
     });
-
-    /* useEffect(()=>{
-
-        const {data, isLoading} = useQuery({
-            queryKey : ["infoPosterKey",info],
-            queryFn : ()=>moviePosterAPI(info?.movieInfo.movieCd || "")
-        });
-
-        setPoster({
-            data,
-            isLoading
-        });
-
-    },[info]) */
 
     useEffect(()=>{
         window.scrollTo(0,0);
@@ -36,21 +22,21 @@ export default function View() {
     return (
         <main className="font-pretendard tracking-custom">
 
-            <div className="h-[450px] bg-black relative md:h-[550px]">
-                <img src="/image/poster.jpg" className="absolute top-0 left-0 w-full h-full object-cover" alt="포스터 이미지" />
-                <dl className="absolute left-1/2 bottom-20 -translate-x-1/2 w-11/12 max-w-screen-xl mx-auto text-white">
-                    <dt className="text-2xl font-bold">{info?.movieInfo.movieNm}</dt>
-                    <dd className="text-base">개봉일 {info?.movieInfo.openDt ? moment(info.movieInfo.openDt).format("YYYY.MM.DD") : ""}</dd>
-                </dl>
-            </div>
+            {
+                info ?
+                    <Poster info={info}/>
+                : null
+            }
 
             <div className="px-4">
                 <div className="container max-w-screen-xl mx-auto py-20">
 
                     <div className="flex flex-wrap gap-5 bg-white p-5 rounded-xl">
-                        <div className="w-52 h-64 bg-point-color relative">
-                            <img src="/image/poster.jpg" className="absolute top-0 left-0 w-full h-full object-cover" alt="포스터 이미지" />
-                        </div>
+                        {
+                            info ?
+                                <SmallPoster info={info}/>
+                            : null
+                        }
                         <div className="flex-none">
                             <p>
                                 <span 
---
+++ src/utils/fetch/poster/ViewGet.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export const viewGetAjax = async (moviecode : string)=>{
+
+    return new Promise<string>((resolve, reject) => { // promise을 만들어 useQuery()에 전달합니다.
+        axios.get(`/api/view/poster/${moviecode}`, { responseType: 'blob' })
+        .then(response => {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                if (typeof reader.result === 'string') {
+                    resolve(reader.result);
+                } else {
+                    reject(new Error('이미지를 문자열로 변환하는 데 문제가 발생했습니다.'));
+                }
+            };
+            reader.readAsDataURL(response.data);
+        })
+        .catch(error => {
+            reject(new Error('포스터를 가져오는데 에러가 발생했습니다.'));
+        });
+    });
+
+}
