65c48aeb96d5a1c9e6df4e2f9461fb22a93dd96f
Kim Ji Yu <spbabo97@naver.com>
2024-04-07 02:18:33 +0900

    :sparkles: 영화 포스터 가져오는 기능 추가

--- src/components/movie/Card.tsx
+++ src/components/movie/Card.tsx
@@ -1,14 +1,16 @@
 
 import { Link } from "react-router-dom";
 import { MovieList } from "../../types/MovieListType";
-import poster from "../../asset/image/poster.jpg";
+import MoivePoster from "../../asset/image/poster.jpg";
+import { listGetAjax } from "../../utils/fetch/poster/ListGet";
+import { useQuery } from "@tanstack/react-query";
 
-export default function Card({item} : {item? : MovieList}) {
+export default function Card({item} : {item : MovieList}) {
     
-    /* const {data : poster,isLoading} = useQuery({
-        queryKey : ["posterKey",item],
-        queryFn : ()=> moviePosterAPI(item.movieCd)
-    }) */
+    const {data : poster,isLoading,isError} = useQuery({
+        queryKey : ["MovieCardPosterKey",item.movieCd],
+        queryFn : ()=>listGetAjax(item.movieCd)
+    })
 
     return (
         <Link to={`/view/${item?.movieCd}`} className="group">
@@ -17,7 +19,13 @@ export default function Card({item} : {item? : MovieList}) {
             >
                 <img 
                     className="absolute left-0 top-0 w-full h-full object-cover transition-transform group-hover:scale-110"
-                    src={poster}
+                    src={ 
+                        isError 
+                            ?
+                                MoivePoster
+                            :
+                                !isLoading && poster ? poster : MoivePoster
+                    }
                     alt="영화 포스터" 
                 />
             </div>
