0eaea6f5c02ee76ee9784b489c0864fbc0821a6a
Kim Ji Yu <spbabo97@naver.com>
2024-04-07 03:01:28 +0900

    :art: 여러가지 warning을 수정하였습니다.

--- src/components/auth/PrivateRoute.tsx
+++ src/components/auth/PrivateRoute.tsx
@@ -1,7 +1,4 @@
-import { useEffect, useState } from "react";
 import { Navigate } from "react-router";
-import { auth } from "../../firebase";
-import { User } from "@firebase/auth";
 import useGetUser from "../../hook/useGetUser";
 
 export default function PrivateRoute({children} : {children : React.ReactNode}) {
